e59bcf4e1c8cd40bc3a7089323d6511e2eb95802
edunham <edunham@mozilla.com>
2015-11-18 16:35:47 -0800

    Start automating gitstat+orglog contributor visualization

---
+++ roles/gitstats/gitstat_settings.py
@@ -0,0 +1,9 @@
+TITLE = 'Rust Organization'
+
+TEMPLATE_FILE = '../orglog/aggregate_template.html'
+INPUT_FILE = '../orglog/TheLog.txt' # None for stdin
+OUTPUT_FILE = 'index.html' # None for stdout
+AUTHOR_FILE = 'authors.txt'
+
+REPO_URL = 'https://github.com/<id>/<repo>/tree/<branch>'
+COMMIT_URL = 'http://github.com/<id>/<repo>/commit/{commit}'
